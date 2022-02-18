ff67c5ce7d5bb46fa6d422499a0225babf085cb3
mlarra <mlarra@va-h3.kzn.21-school.ru>
2022-02-18 14:56:51 +0300

    18-02-2022

--- push_swap_bonus.c
+++ push_swap_bonus.c
@@ -6,97 +6,11 @@
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 11:52:03 by mlarra            #+#    #+#             */
-/*   Updated: 2022/02/15 17:35:34 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/02/16 10:42:06 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap_bonus.h"
-
-// t_list	*ft_fill_stack(char **argv)
-// {
-// 	int		i;
-// 	int		j;
-// 	t_list	*new;
-// 	t_list	*stack;
-// 	char	**words;
-
-// 	i = 1;
-// 	stack = NULL;
-// 	while (argv[i])
-// 	{
-// 		words = ft_split(argv[i], ' ');
-// 		j = 0;
-// 		while (words[j])
-// 		{
-// 			new = ft_lstnew(my_atoi(words[j]));
-// 			ft_lstadd_back(&stack, new);
-// 			free(words[j]);
-// 			j++;
-// 		}
-// 		free(words);
-// 		i++;
-// 	}
-// 	return (stack);
-// }
-
-// int	ft_get_length(char **s)
-// {
-// 	int		k;
-// 	int		i;
-// 	int		j;
-// 	char	**wd;
-
-// 	k = 0;
-// 	i = 1;
-// 	while (s[i])
-// 	{
-// 		j = 0;
-// 		wd = ft_split((const char *)s[i], ' ');
-// 		while (wd[j])
-// 		{
-// 			k++;
-// 			free(wd[j]);
-// 			j++;
-// 		}
-// 		free(wd);
-// 		i++;
-// 	}
-// 	return (k);
-// }
-
-// int	get_next_line(char **line)
-// {
-// 	int		i;
-// 	int		j;
-// 	char	*buf;
-
-// 	buf = (char*) malloc(sizeof(char) * 4);
-// 	if (!buf)
-// 		ft_exit_argc();
-// 	i = 0;
-// 	j = read(0, &buf[i], 1);
-// 	while (j > 0 && buf[i] != '\0' && buf[i] != '\n')
-// 	{
-// 		i++;
-// 		j = read(0, &buf[i], 1);
-// 	}
-// 	buf[i] = '\0';
-// 	*line = buf;
-// 	return (j);
-// }
-
-// int	ft_lstsize(t_list *lst)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (lst)
-// 	{
-// 		lst = lst->next;
-// 		i++;
-// 	}
-// 	return (i);
-// }
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
