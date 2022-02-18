d63de59fa5398a0acf69dfcbea97dce6475def45
mlarra <mlarra@va-h3.kzn.21-school.ru>
2022-02-18 14:58:19 +0300

    add exit_finc_bonus

--- exit_func_bonus.c
+++ exit_func_bonus.c
@@ -6,27 +6,30 @@
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 15:39:00 by mlarra            #+#    #+#             */
-/*   Updated: 2022/02/15 17:35:41 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/02/18 14:03:51 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap_bonus.h"
 
-void	ft_exit_symbol(t_list *stack)
-{
-	ft_lstclear(&stack);
-	write(2, "Error\n", 6);
-	exit(1);
-}
+// void	ft_exit_symbol(t_list *stack)
+// {
+// 	ft_lstclear(&stack);
+// 	write(2, "Error\n", 6);
+// 	exit(1);
+// }
 
 void	ft_exit_argv(void)
 {
+// write(2, "ft_exit_argv\n", 13);
 	write(2, "Error\n", 6);
 	exit(1);
 }
 
 void	ft_exit_malloc(void)
 {
+// write(2, "ft_exit_malloc\n", 15);
+
 	write(2, "Memory don't allocated\n", 23);
 	exit(1);
 }
