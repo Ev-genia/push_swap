65e729a019a2e601a76f5b553d6f8707a4b2e816
mlarra <mlarra@va-h3.kzn.21-school.ru>
2022-02-18 15:02:51 +0300

    norm OK

--- exit_func_bonus.c
+++ exit_func_bonus.c
@@ -6,30 +6,20 @@
 /*   By: mlarra <mlarra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 15:39:00 by mlarra            #+#    #+#             */
-/*   Updated: 2022/02/18 14:03:51 by mlarra           ###   ########.fr       */
+/*   Updated: 2022/02/18 15:00:51 by mlarra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap_bonus.h"
 
-// void	ft_exit_symbol(t_list *stack)
-// {
-// 	ft_lstclear(&stack);
-// 	write(2, "Error\n", 6);
-// 	exit(1);
-// }
-
 void	ft_exit_argv(void)
 {
-// write(2, "ft_exit_argv\n", 13);
 	write(2, "Error\n", 6);
 	exit(1);
 }
 
 void	ft_exit_malloc(void)
 {
-// write(2, "ft_exit_malloc\n", 15);
-
 	write(2, "Memory don't allocated\n", 23);
 	exit(1);
 }
